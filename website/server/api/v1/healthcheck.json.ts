8b56bc59d75227f6771ead4a4f9b48d3dad39ab0
Matthew Holloway <matthew@holloway.co.nz>
2026-04-24 21:36:10 +1200

    feat: healthcheck and systemcheck

--- website/server/api/v1/healthcheck.json.ts
+++ website/server/api/v1/healthcheck.json.ts
@@ -1,4 +1,3 @@
-import https from 'https'
 import { DateTime } from 'luxon'
 import { usePublicSiteUrlOrigin } from '~/utils/url'
 
@@ -7,67 +6,17 @@ import { usePublicSiteUrlOrigin } from '~/utils/url'
  */
 export default defineEventHandler(async (event) => {
   const publicSiteUrlOrigin = usePublicSiteUrlOrigin()
-
-  const pathsToCheck = [
-    '/',
-    '/about/',
-    '/robots.txt',
-    '/sitemap.xml',
-    '/api/v1/queue/index.json',
-    '/api/v1/final-review/index.json'
-  ]
-
-  const checks = await Promise.all(pathsToCheck.map(pathToCheck => httpCheck(
-    new URL(pathToCheck, publicSiteUrlOrigin).toString()
-  )))
-
-  const isOk = checks.every(check => check.ok)
-
-  const failedPathsMessage = checks.map((check) => {
-    if (!check.ok) {
-      return `${check.url} (HTTP ${check.statusCode})`
-    }
-    return undefined
-  }).filter(Boolean).join(', ')
-
   const timestampIso = DateTime.now().toISO()
 
   setResponseHeader(event, 'Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
   setResponseHeader(event, 'Pragma', 'no-cache')
   setResponseHeader(event, 'Expires', '0')
 
-  setResponseStatus(event, isOk ? 200 : 500)
+  setResponseStatus(event, 200)
 
   return {
-    ok: isOk,
-    timestampIso,
-    message: isOk ? undefined : `Health check failed on ${publicSiteUrlOrigin}: ${failedPathsMessage}`,
+    ok: true,
+    publicSiteUrlOrigin,
+    timestampIso
   }
 })
-
-type Check = {
-  ok: boolean
-  url: string
-  statusCode: number
-}
-
-const httpCheck = (url: string) => {
-  return new Promise<Check>((resolve, reject) => {
-    https
-      .request(url, { method: 'HEAD' }, (res) => {
-        const { statusCode } = res
-        if (statusCode === undefined) {
-          reject()
-          return
-        }
-        resolve({
-          // using fetch response 'ok' definition of ok
-          // https://developer.mozilla.org/en-US/docs/Web/API/Response/ok
-          ok: statusCode >= 200 && statusCode <= 299,
-          url,
-          statusCode
-        })
-      }).on('error', (_err) => reject())
-      .end()
-  })
-}
---
+++ website/server/api/v1/systemcheck.json.ts
@@ -0,0 +1,81 @@
+import https from 'https'
+import { DateTime } from 'luxon'
+import { usePublicSiteUrlOrigin } from '~/utils/url'
+
+/**
+ * Health check
+ */
+export default defineEventHandler(async (event) => {
+  const publicSiteUrlOrigin = usePublicSiteUrlOrigin()
+
+  const pathsToCheck = [
+    '/',
+    '/about/',
+    '/robots.txt',
+    '/sitemap.xml',
+    '/api/v1/queue/index.json',
+    '/api/v1/final-review/index.json'
+  ]
+
+  const checks = await Promise.all(
+    pathsToCheck.map((pathToCheck) =>
+      httpCheck(new URL(pathToCheck, publicSiteUrlOrigin).toString())
+    )
+  )
+
+  const isOk = checks.every((check) => check.ok)
+
+  const failedPathsMessage = checks
+    .map((check) => {
+      if (!check.ok) {
+        return `${check.url} (HTTP ${check.statusCode})`
+      }
+      return undefined
+    })
+    .filter(Boolean)
+    .join(', ')
+
+  const timestampIso = DateTime.now().toISO()
+
+  setResponseHeader(event, 'Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
+  setResponseHeader(event, 'Pragma', 'no-cache')
+  setResponseHeader(event, 'Expires', '0')
+
+  setResponseStatus(event, isOk ? 200 : 500)
+
+  return {
+    ok: isOk,
+    timestampIso,
+    message: isOk
+      ? undefined
+      : `Health check failed on ${publicSiteUrlOrigin}: ${failedPathsMessage}`
+  }
+})
+
+type Check = {
+  ok: boolean
+  url: string
+  statusCode: number
+}
+
+const httpCheck = (url: string) => {
+  return new Promise<Check>((resolve, reject) => {
+    https
+      .request(url, { method: 'HEAD' }, (res) => {
+        const { statusCode } = res
+        if (statusCode === undefined) {
+          reject()
+          return
+        }
+        resolve({
+          // using fetch response 'ok' definition of ok
+          // https://developer.mozilla.org/en-US/docs/Web/API/Response/ok
+          ok: statusCode >= 200 && statusCode <= 299,
+          url,
+          statusCode
+        })
+      })
+      .on('error', (_err) => reject())
+      .end()
+  })
+}
