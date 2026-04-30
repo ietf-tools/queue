067afe7dda9380f935d4670c05647f1ae694b0a1
Matthew Holloway <matthew@holloway.co.nz>
2026-04-30 16:34:03 +1200

    fix: remove leading 'd'

--- website/app/components/HeaderNavMobile.vue
+++ website/app/components/HeaderNavMobile.vue
@@ -1,9 +1,6 @@
 <template>
   <DialogRoot v-model:open="isOpen">
-    <DialogTrigger
-      aria-label="Menu"
-      class="absolute top-0 right-0 block px-3 py-4.5 block lg:hidden"
-    >
+    <DialogTrigger aria-label="Menu" class="absolute top-0 right-0 block px-3 py-4.5 block lg:hidden">
       <GraphicsHamburgerMenu />
     </DialogTrigger>
     <DialogPortal>
@@ -11,10 +8,7 @@
       <DialogContent :class="// needs overflow-y-scroll to force scrollbars, to ensure same page width as the main view
         'absolute inset-0 z-50 bg-blue-900 text-white dark:bg-blue-950 dark:text-white overflow-y-scroll h-full'">
         <DialogTitle>
-          <button
-            type="button"
-            class="flex justify-between w-full py-5 px-4 items-center"
-          >
+          <button type="button" class="flex justify-between w-full py-5 px-4 items-center">
             <GraphicsHeaderLogoMobileMenu />
             <DialogClose>
               <GraphicsClose class="text-white" />
@@ -23,96 +17,44 @@
         </DialogTitle>
         <div class="flex flex-col">
           <Accordion>
-            <template
-              v-for="(item, index) in menuData"
-              :key="index.toString()"
-            >
-              <Anchor
-                v-if="item.href"
-                :href="item.href"
-                :class="MENU_ITEM_CLASS"
-                @click="isOpen = false"
-              >
-                <GraphicsChevron
-                  v-if="item.hideDropdownIconDesktop"
-                  class="absolute right-0 mt-1 mr-4 size-4 -rotate-90 text-blue-100"
-                />d
+            <template v-for="(item, index) in menuData" :key="index.toString()">
+              <Anchor v-if="item.href" :href="item.href" :class="MENU_ITEM_CLASS" @click="isOpen = false">
+                <GraphicsChevron v-if="item.hideDropdownIconDesktop"
+                  class="absolute right-0 mt-1 mr-4 size-4 -rotate-90 text-blue-100" />
                 {{ item.label }}
               </Anchor>
-              <AccordionItem
-                v-else
-                :id="index.toString()"
-                :trigger-text="item.label"
-              >
+              <AccordionItem v-else :id="index.toString()" :trigger-text="item.label">
                 <ul class="ml-4">
-                  <li
-                    v-for="(level0, childIndex) in item.children"
-                    :key="childIndex"
-                  >
-                    <Anchor
-                      v-if="level0.href"
-                      :href="level0.href"
-                      :class="MENU_ITEM_CLASS"
-                      @click="isOpen = false"
-                    >
+                  <li v-for="(level0, childIndex) in item.children" :key="childIndex">
+                    <Anchor v-if="level0.href" :href="level0.href" :class="MENU_ITEM_CLASS" @click="isOpen = false">
                       {{ level0.label }}
                     </Anchor>
-                    <button
-                      v-else-if="level0.click"
-                      type="button"
-                      :aria-label="level0.activeLabelFn?.()"
-                      :class="[MENU_ITEM_CLASS, 'flex items-center']"
-                      @click="
+                    <button v-else-if="level0.click" type="button" :aria-label="level0.activeLabelFn?.()"
+                      :class="[MENU_ITEM_CLASS, 'flex items-center']" @click="
                         (e: MouseEvent) => {
                           level0.click?.(e)
                           isOpen = false
                         }
-                      "
-                    >
-                      <Icon
-                        v-if="level0.icon"
-                        :name="level0.icon"
-                      />
-                      <Icon
-                        v-if="level0.isActiveFn?.()"
-                        name="fluent:checkmark-12-filled"
-                        class="inline-block w-[14px] h-[14px] mr-1"
-                      />
-                      <span
-                        v-if="
-                          level0.isActiveFn && !level0.isActiveFn() // render blank space if isActiveFn()===false
-                        "
-                        class="inline-block w-[14px] h-[14px] mr-1"
-                      />
+                      ">
+                      <Icon v-if="level0.icon" :name="level0.icon" />
+                      <Icon v-if="level0.isActiveFn?.()" name="fluent:checkmark-12-filled"
+                        class="inline-block w-[14px] h-[14px] mr-1" />
+                      <span v-if="
+                        level0.isActiveFn && !level0.isActiveFn() // render blank space if isActiveFn()===false
+                      " class="inline-block w-[14px] h-[14px] mr-1" />
                       {{ level0.label }}
                     </button>
                     <Accordion v-else>
-                      <AccordionItem
-                        :id="index.toString()"
-                        :key="index"
-                        :trigger-text="level0.label"
-                        :style-depth="2"
-                      >
+                      <AccordionItem :id="index.toString()" :key="index" :trigger-text="level0.label" :style-depth="2">
                         <ul class="ml-4">
-                          <li
-                            v-for="(level1, level1Index) in level0.children"
-                            :key="level1Index"
-                          >
+                          <li v-for="(level1, level1Index) in level0.children" :key="level1Index">
                             <template v-if="level1.href">
-                              <Anchor
-                                v-if="!level1.noSpaLink"
-                                :href="level1.href"
-                                :class="MENU_ITEM_CLASS"
-                                @click="isOpen = false"
-                              >
+                              <Anchor v-if="!level1.noSpaLink" :href="level1.href" :class="MENU_ITEM_CLASS"
+                                @click="isOpen = false">
                                 {{ level1.label }}
                               </Anchor>
-                              <a
-                                v-else-if="level1.noSpaLink"
-                                :href="level1.href"
-                                :class="MENU_ITEM_CLASS"
-                                @click="isOpen = false"
-                              >
+                              <a v-else-if="level1.noSpaLink" :href="level1.href" :class="MENU_ITEM_CLASS"
+                                @click="isOpen = false">
                                 {{ level1.label }}
                               </a>
                             </template>
